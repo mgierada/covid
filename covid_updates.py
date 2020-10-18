411f0eb4f7d43db647e64811ce060623c972ff0b
Maciej Gierada <maciek.gierada@gmail.com>
2020-10-17 17:29:57 -0700

    created a class that will contain all methods

--- covid_updates.py
+++ covid_updates.py
@@ -2,10 +2,15 @@ import os
 from urllib import request
 
 
-def get_data(link):
-    data = os.path.join(os.getcwd(), 'covid_dataset.csv')
-    request.urlretrieve(link, data)
+class Covid():
+    def __init__(self, link):
+        self.link = link
+
+    def get_data(self):
+        data = os.path.join(os.getcwd(), 'covid_dataset.csv')
+        request.urlretrieve(self.link, data)
 
 
 link = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
-get_data(link)
+
+Covid(link).get_data()
