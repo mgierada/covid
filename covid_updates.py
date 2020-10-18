3bf2b1f3ce8f50a76f2eb2e3d66f12215d6d3fbe
Maciej Gierada <maciek.gierada@gmail.com>
2020-10-17 17:27:34 -0700

    get_data works - downloads the latest covid dataset

--- covid_updates.py
+++ covid_updates.py
@@ -1,2 +1,11 @@
-def get_data():
-    pass
+import os
+from urllib import request
+
+
+def get_data(link):
+    data = os.path.join(os.getcwd(), 'covid_dataset.csv')
+    request.urlretrieve(link, data)
+
+
+link = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
+get_data(link)
